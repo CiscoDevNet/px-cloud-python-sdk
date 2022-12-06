f34980c4d2e718ff692e3a332362981fa399398c
shivamch <shivamch@cisco.com>
2022-12-06 20:11:54 +0530

    OSV Models

---
+++ tests/px_osv_v1_python_sdk/model/softwareGroupModel.py
@@ -0,0 +1,20 @@
+software_group_model = {
+    'totalCount': int,
+    'items': [
+        {
+            "assetCount": str,
+            "sourceId": str,
+            "sourceSystemId": str,
+            "selectedRelease": str,
+            "productFamily": str,
+            "softwareGroupName": str,
+            "softwareGroupId": str,
+            "suggestionId": str,
+            "suggestions": str,
+            "riskLevel": str,
+            "softwareType": str,
+            "currentReleases": str,
+            "managedBy": str
+        }
+    ]
+}
